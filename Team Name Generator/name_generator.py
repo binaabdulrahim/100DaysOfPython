3185eec3dc4ffcd65a14c63bac8a0dbcee4d6d8f
Bina Abdul Rahim <binafshaii@icloud.com>
2022-03-04 03:57:22 -0500

    Team Name Gen project

---
+++ Team Name Generator/name_generator.py
@@ -0,0 +1,7 @@
+
+print("Hello")
+#input function - a prompt for the user
+name = input("What is your name? ")
+print(name)
+soccer_team = input ("What is your favorite soccer team? ")
+print("Team name could be " + name + "" + soccer_team)
